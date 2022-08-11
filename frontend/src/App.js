3cbf996aa44e8cb29cbec16744a9e8235a8d4d65
Mr-Chunky <dean.carter98@yahoo.com>
2022-08-11 19:47:51 +0200

    Colour background; add logo to page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,11 @@
+import logo from "./images/thrift-e-logo.svg";
+
 function App() {
-  return <div>Placeholder</div>;
+  return (
+    <div>
+      <img alt="thrift-e-logo" src={logo} />
+    </div>
+  );
 }
 
 export default App;
