3de6b756d01ed888b26a7bd4eeb328ad139e6552
Mr-Chunky <dean.carter98@yahoo.com>
2022-08-11 20:22:53 +0200

    Edit image to center of page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,9 @@ import logo from "./images/thrift-e-logo.svg";
 function App() {
   return (
     <div>
-      <img alt="thrift-e-logo" src={logo} />
+      <div id="login-logo-holder">
+        <img className="logo" alt="thrift-e-logo" src={logo} />
+      </div>
     </div>
   );
 }
