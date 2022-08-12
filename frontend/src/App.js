c12fe7d2f870b8bd940aaa8d834c570a287ffa05
Mr-Chunky <dean.carter98@yahoo.com>
2022-08-12 21:00:37 +0200

    Add username input field

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import LoginLogo from "./components/LoginLogo";
 import LoginHeader from "./components/LoginHeader";
+import UsernameInputField from "./components/UsernameInputField";
 
 function App() {
   return (
     <div>
       <LoginLogo />
       <LoginHeader />
+      <UsernameInputField />
     </div>
   );
 }
---
+++ frontend/src/components/UsernameInputField.js
@@ -0,0 +1,17 @@
+function UsernameInputField() {
+  return (
+    <form>
+      <div className="form-group">
+        <label for="login-username-input">Username</label>
+        <input
+          type="text"
+          className="form-control"
+          id="login-username-input"
+          placeholder="Enter Username"
+        ></input>
+      </div>
+    </form>
+  );
+}
+
+export default UsernameInputField;
