995edc03cac05cda2f7d93ebc35216026c6bd117
Mr-Chunky <dean.carter98@yahoo.com>
2022-08-12 20:35:41 +0200

    Add LoginHeader to Login Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import LoginLogo from "./components/LoginLogo";
+import LoginHeader from "./components/LoginHeader";
 
 function App() {
   return (
     <div>
       <LoginLogo />
+      <LoginHeader />
     </div>
   );
 }
---
+++ frontend/src/components/LoginHeader.js
@@ -0,0 +1,9 @@
+function LoginHeader() {
+  return (
+    <div id="login-header-holder">
+      <h1 className="page-heading">Account Login</h1>
+    </div>
+  );
+}
+
+export default LoginHeader;
