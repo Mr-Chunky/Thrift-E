333f5430874fe0d88524b46991488ef3fca3fe9f
Mr-Chunky <dean.carter98@yahoo.com>
2022-08-15 16:18:19 +0200

    Refactor classes; add input fields

--- frontend/src/components/login_components/LoginInputFields.js
+++ frontend/src/components/login_components/LoginInputFields.js
@@ -1,35 +1,29 @@
-import modifiers from "./Login.module.css";
-
 function LoginInputFields() {
   return (
     <form>
       <div className="form-group">
-        <label
-          className={modifiers.login_input_label}
-          for="login-username-input"
-        >
-          Username
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="login-username-input"
-          placeholder="Enter Username"
-        ></input>
-      </div>
-      <div className="form-group">
-        <label
-          className={modifiers.login_input_label}
-          for="login-password-input"
-        >
-          Password
-        </label>
-        <input
-          type="password"
-          className="form-control"
-          id="login-password-input"
-          placeholder="Enter Password"
-        ></input>
+        <div className="col">
+          <label className="user-input-label" for="login-username-input">
+            Username
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="login-username-input"
+            placeholder="Enter Username"
+          ></input>
+        </div>
+        <div className="col">
+          <label className="user-input-label" for="login-password-input">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="login-password-input"
+            placeholder="Enter Password"
+          ></input>
+        </div>
       </div>
     </form>
   );
--- frontend/src/pages/CreateAccount.js
+++ frontend/src/pages/CreateAccount.js
@@ -1,11 +1,13 @@
 import CreateAccountLogo from "../components/create_account_components/CreateAccountLogo";
 import CreateAccountHeader from "../components/create_account_components/CreateAccountHeader";
+import CreateAccountInputFields from "../components/create_account_components/CreateAccountInputFields";
 
 function CreateAccountPage() {
   return (
     <div>
       <CreateAccountLogo />
       <CreateAccountHeader />
+      <CreateAccountInputFields />
     </div>
   );
 }
