66b9cc01c2dc888fc577d2fc17e424d3cc3104a5
Mr-Chunky <dean.carter98@yahoo.com>
2022-10-27 08:30:49 +0200

    Format Game Card documentation for readability

--- frontend/src/components/ui/GameCard.js
+++ frontend/src/components/ui/GameCard.js
@@ -8,31 +8,31 @@ props.name -- The name of the game as advertised on Steam
 props.steam_appid -- The unique ID of the game stored on Steam
 props.short_description -- A short "about" statement for the game
 props.genres -- An array of objects containing genres applied to the game, where:
-    * id = object[0]
-    * description = object[1]
+    * id = object.id
+    * description = object.description
 props.release_date -- An object containing two items, "Coming Soon (bool)" and "Release Date (Date)"
 props.developers -- An array containing the studios that worked on the game
 props.publishers -- An array containing the entities with publishing rights to the game
 props.price_overview -- An object containing details such as:
-    * currency
-    * base price
-    * net price
-    * discount percentage granted
-    * initial formatting rules applied
-    * final formatting rules applied for presentation
+    * currency: units used in the local region that the API was called from
+    * initial: price before discount applied
+    * final: net price after discount applied
+    * discount_percent: integer value of percentage discount granted
+    * initial_formatted: pre-formatting before further processing
+    * final_formatted: final result of formatting applied to currency
 ------------------------------------------------------------------
 */
 
 function GameCard(props) {
   return (
-    <li className={modifiers.gameCard}>
+    <li key={props.steam_appid} className={modifiers.gameCard}>
       <div>
-        <img src={props.header_image} alt="" />
+        <img src={props.header_image} alt="Represents the game" />
       </div>
       <div>
-        <h3>GAME TITLE</h3>
-        <p> GAME DESCRIPTION</p>
-        <span>GAME PRICE</span>
+        <h3>{props.name}</h3>
+        <p>{props.short_description}</p>
+        <span>{props.price_overview}</span>
       </div>
       <div>
         <button>Add to Favourites!</button>
