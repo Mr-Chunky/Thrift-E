7ff6fc82bdfd9fbd32519a00e39f0dc97878c3e1
Mr-Chunky <dean.carter98@yahoo.com>
2022-10-28 11:54:58 +0200

    Expand game card on click, add game attributes

--- frontend/src/components/ui/GameCard.js
+++ frontend/src/components/ui/GameCard.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import modifiers from "./UI.module.css";
 
 /*
@@ -24,24 +25,52 @@ props.price_overview -- An object containing details such as:
 */
 
 function GameCard(props) {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const handleSelection = () => {
+    if (!isSelected) {
+      setIsSelected(true);
+    } else if (isSelected) {
+      setIsSelected(false);
+    }
+  };
+
   return (
     <li className={modifiers.gameCard}>
-      <div>
-        <img
-          id={modifiers.gameCardImage}
-          src={props.header_image}
-          alt="Represents the game"
-        />
-      </div>
-      <div className={modifiers.cardContent}>
-        <h3>{props.name}</h3>
-        <p>{props.short_description}</p>
-        <span>{props.price_overview.final_formatted}</span>
+      <div onClick={handleSelection} style={{ cursor: "pointer" }}>
+        <div>
+          <img
+            id={modifiers.gameCardImage}
+            src={props.header_image}
+            alt="Represents the game"
+          />
+        </div>
+        <div className={modifiers.cardContent}>
+          <h3>{props.name}</h3>
+          {isSelected ? (
+            <p>{props.short_description}</p>
+          ) : (
+            props.publishers.map((publisher) => (
+              <h6 key={publisher}>{publisher}</h6>
+            ))
+          )}
+          <span className={modifiers.gameAttributes}>
+            {props.price_overview.final_formatted}
+          </span>
+          <span
+            className={modifiers.gameAttributes}
+            style={{ paddingLeft: "3.5em" }}
+          >
+            {props.genres[0].description}/{props.genres[1].description}
+          </span>
+        </div>
       </div>
       <div className="center-button-bar-holder">
         <button
           id={modifiers.favouritesButton}
           className="btn btn-outline-secondary"
+          type="button"
+          //onClick={}
         >
           Add to Favourites!
         </button>
