4de85e8ceda5853fb566be468f3ceddd33174358
Mr-Chunky <dean.carter98@yahoo.com>
2022-11-06 21:51:18 +0200

    Preparing component for POST request of game

---
+++ frontend/src/components/search_games_components/SearchGamesFavouritedGames.js
@@ -0,0 +1,11 @@
+import FavouritedGameCard from "../ui/FavouritedGameCard";
+
+function SearchGamesFavouritedGames() {
+  return (
+    <section>
+      <FavouritedGameCard />
+    </section>
+  );
+}
+
+export default SearchGamesFavouritedGames;
