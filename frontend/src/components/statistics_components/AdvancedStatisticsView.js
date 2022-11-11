396cfdf2e8e9964afaac960d205b9906bda6bb4f
Mr-Chunky <dean.carter98@yahoo.com>
2022-11-11 21:22:26 +0200

    Add table with stats to advanced stats page, fix text alignment on simple stats page

--- frontend/src/components/statistics_components/AdvancedStatisticsView.js
+++ frontend/src/components/statistics_components/AdvancedStatisticsView.js
@@ -1,7 +1,32 @@
-import GeneralUICard from "../ui/GeneralUICard";
-
+import modifiers from "./AdvancedStatistics.module.css";
 function AdvancedStatisticsView() {
-  return <div>Placeholder!</div>;
+  return (
+    <div style={{ margin: "0 auto" }}>
+      <table>
+        <tr>
+          <th>Most-Favourited Game</th>
+          <th>Lowest Sale Price</th>
+          <th>Most Popular Vendor</th>
+          <th>Most-Favourited Genre</th>
+          <th>Least Popular Vendor</th>
+        </tr>
+        <tr className={modifiers.tableRowOne}>
+          <td>Placeholder</td>
+          <td>Placeholder</td>
+          <td>Placeholder</td>
+          <td>Placeholder</td>
+          <td>Placeholder</td>
+        </tr>
+        <tr className={modifiers.tableRowTwo}>
+          <td>Number</td>
+          <td>Number</td>
+          <td>Number</td>
+          <td>Number</td>
+          <td>Number</td>
+        </tr>
+      </table>
+    </div>
+  );
 }
 
 export default AdvancedStatisticsView;
