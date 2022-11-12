653cbd9f29d23fcea7d5fcffdf5780f8a561cba5
Mr-Chunky <dean.carter98@yahoo.com>
2022-11-13 01:28:05 +0200

    Implement statistics fetching from backend

--- frontend/src/components/statistics_components/SimpleStatisticsView.js
+++ frontend/src/components/statistics_components/SimpleStatisticsView.js
@@ -2,7 +2,7 @@ import modifiers from "./SimpleStatistics.module.css";
 import GeneralUICard from "../ui/GeneralUICard";
 import { useNavigate } from "react-router-dom";
 
-function SimpleStatisticsView() {
+function SimpleStatisticsView(props) {
   const navigate = useNavigate();
 
   const handleAdvancedStatsNavigation = () => {
@@ -15,25 +15,31 @@ function SimpleStatisticsView() {
         <div className={modifiers.firstRowStats}>
           <div id={modifiers.firstStatistic}>
             <h4 className={modifiers.statsHeadings}>Most-Favourited Game</h4>
-            <span className={modifiers.statsText}>Placeholder Data</span>
+            <span className={modifiers.statsText}>
+              {props.mostFavouritedGame.title}
+            </span>
           </div>
           <div id={modifiers.secondStatistic}>
             <h4 className={modifiers.statsHeadings}>Lowest Sale Price</h4>
-            <span className={modifiers.statsText}>Placeholder Data</span>
+            <span className={modifiers.statsText}>
+              {props.lowestPriceGame.lowestPrice}
+            </span>
           </div>
           <div id={modifiers.thirdStatistic}>
             <h4 className={modifiers.statsHeadings}>Most Popular Vendor</h4>
-            <span className={modifiers.statsText}>Placeholder Data</span>
+            <span className={modifiers.statsText}>Steam</span>
           </div>
         </div>
         <div className={modifiers.secondRowStats}>
           <div id={modifiers.fourthStatistic}>
             <h4 className={modifiers.statsHeadings}>Most-Favourited Genre</h4>
-            <span className={modifiers.statsText}>Placeholder Data</span>
+            <span className={modifiers.statsText}>
+              {props.mostFavouritedGenre.genre}
+            </span>
           </div>
           <div id={modifiers.fifthStatistic}>
             <h4 className={modifiers.statsHeadings}>Least Popular Vendor</h4>
-            <span className={modifiers.statsText}>Placeholder Data</span>
+            <span className={modifiers.statsText}>Steam</span>
           </div>
         </div>
         <div
--- frontend/src/pages/SimpleStatistics.js
+++ frontend/src/pages/SimpleStatistics.js
@@ -1,13 +1,115 @@
 import SimpleStatisticsHeader from "../components/statistics_components/SimpleStatisticsHeader";
 import SimpleStatisticsView from "../components/statistics_components/SimpleStatisticsView";
 import NavBar from "../components/ui/NavBar";
+import { useEffect, useState } from "react";
 
 function SimpleStatisticsPage() {
+  const [mostFavouritedGame, setMostFavouritedGame] = useState();
+  const [lowestPriceGame, setLowestPriceGame] = useState();
+  const [mostFavouritedGenre, setMostFavouritedGenre] = useState();
+
+  // Get most favourited game from backend API
+  useEffect(() => {
+    try {
+      fetch(
+        `http://localhost/SteamService/api/steam/game/most-favourited`
+      ).then(async (response) => {
+        console.log(
+          `>Simple Stats Page: HTTP Status Code - ${response.status}`
+        );
+        if (!response.ok) {
+          console.log(await response.json());
+          throw new Error(
+            `>Simple Stats Page: Error! Current Status - ${response.status}`
+          );
+        } else if (response.ok) {
+          let gameInfoObject = await response.json();
+          let finalInfo = JSON.stringify(gameInfoObject, undefined, 2);
+          let finalInfoObject = JSON.parse(finalInfo);
+
+          console.warn(
+            `>Simple Stats Page: Most Favourited Game Info Received - \n------------\n${finalInfo}`
+          );
+
+          setMostFavouritedGame(finalInfoObject);
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  // Get game with lowest price from backend API
+  useEffect(() => {
+    try {
+      fetch(`http://localhost/SteamService/api/steam/game/lowest-price`).then(
+        async (response) => {
+          console.log(
+            `>Simple Stats Page: HTTP Status Code - ${response.status}`
+          );
+          if (!response.ok) {
+            console.log(await response.json());
+            throw new Error(
+              `>Simple Stats Page: Error! Current Status - ${response.status}`
+            );
+          } else if (response.ok) {
+            let gameInfoObject = await response.json();
+            let finalInfo = JSON.stringify(gameInfoObject, undefined, 2);
+            let finalInfoObject = JSON.parse(finalInfo);
+
+            console.warn(
+              `>Simple Stats Page: Lowest-Price Game Info Received - \n------------\n${finalInfo}`
+            );
+
+            setLowestPriceGame(finalInfoObject);
+          }
+        }
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  // Get most favourited genre from backend API
+  useEffect(() => {
+    try {
+      fetch(
+        `http://localhost/SteamService/api/steam/genre/most-favourited`
+      ).then(async (response) => {
+        console.log(
+          `>Simple Stats Page: HTTP Status Code - ${response.status}`
+        );
+        if (!response.ok) {
+          console.log(await response.json());
+          throw new Error(
+            `>Simple Stats Page: Error! Current Status - ${response.status}`
+          );
+        } else if (response.ok) {
+          let gameInfoObject = await response.json();
+          let finalInfo = JSON.stringify(gameInfoObject, undefined, 2);
+          let finalInfoObject = JSON.parse(finalInfo);
+
+          console.warn(
+            `>Simple Stats Page: Most Favourited Genre Info Received - \n------------\n${finalInfo}`
+          );
+
+          setMostFavouritedGenre(finalInfoObject);
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   return (
     <div>
       <NavBar />
       <SimpleStatisticsHeader />
-      <SimpleStatisticsView />
+      <SimpleStatisticsView
+        mostFavouritedGame={mostFavouritedGame}
+        lowestPriceGame={lowestPriceGame}
+        mostFavouritedGenre={mostFavouritedGenre}
+      />
     </div>
   );
 }
