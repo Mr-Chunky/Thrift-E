3cf17b5e6820be4f17ef756e3b9d0e6d3cd772b4
Mr-Chunky <dean.carter98@yahoo.com>
2022-11-14 23:35:04 +0200

    Add documentation comments to list items

--- frontend/src/components/ui/FavouritedGameListItem.js
+++ frontend/src/components/ui/FavouritedGameListItem.js
@@ -1,3 +1,17 @@
-function FavouritedGameListItem() {}
+/*
+Possible options to choose from when displaying this GameCard:
+------------------------------------------------------------------
+props._gameId -- ID of the internally stored game in the MySQL database
+props.title -- The name of the game as stored in the MySQL database
+props.genre -- The genre of the game as stored in the MySQL database
+props.image -- The URL of the image associated with the game as stored in the MySQL database
+props.release_date -- The release date of the game as stored in the MySQL database
+props.localPrice -- The price of the game as stored in the MySQL database (move decimal place 2 places to the left)
+props.publisher -- The publisher of the game as stored in the MySQL database
+props.saleStatus -- Returns the sale status of the game as stored in the MySQL database, where 1 = Sale & 0 = No Sale
+------------------------------------------------------------------
+*/
+
+function FavouritedGameListItem(props) {}
 
 export default FavouritedGameListItem;
--- frontend/src/components/ui/SearchedGameListItem.js
+++ frontend/src/components/ui/SearchedGameListItem.js
@@ -1,3 +1,28 @@
-function SearchedGameListItem() {}
+/*
+Possible options to choose from when displaying this GameCard:
+------------------------------------------------------------------
+props.header_image -- A URL pointing to the game's thumbnail image
+props.name -- The name of the game as advertised on Steam
+props.steam_appid -- The unique ID of the game stored on Steam
+props.short_description -- A short "about" statement for the game
+props.genres -- An array of objects containing genres applied to the game, where:
+    * id = object.id
+    * description = object.description
+props.release_date -- An object containing two items:
+    * coming_soon (bool)
+    * date (Date)
+props.developers -- An array containing the studios that worked on the game
+props.publishers -- An array containing the entities with publishing rights to the game
+props.price_overview -- An object containing details such as:
+    * currency: units used in the local region that the API was called from
+    * initial: price before discount applied
+    * final: net price after discount applied
+    * discount_percent: integer value of percentage discount granted
+    * initial_formatted: pre-formatting before further processing
+    * final_formatted: final result of formatting applied to currency
+------------------------------------------------------------------
+*/
+
+function SearchedGameListItem(props) {}
 
 export default SearchedGameListItem;
