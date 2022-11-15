1a5c550d5d6404a83d4458a6cd07bd0acc4dfb46
Mr-Chunky <dean.carter98@yahoo.com>
2022-11-15 22:04:58 +0200

    Implement searched game list item functionality

--- frontend/src/components/ui/SearchedGameListItem.js
+++ frontend/src/components/ui/SearchedGameListItem.js
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import modifiers from "./UI.module.css";
+
 /*
 Possible options to choose from when displaying this GameCard:
 ------------------------------------------------------------------
@@ -24,9 +27,141 @@ props.price_overview -- An object containing details such as:
 */
 
 function SearchedGameListItem(props) {
+  const userId = window.localStorage.getItem("userId");
+  let saleStatus;
+  props.price_overview.initial - props.price_overview.final > 0
+    ? (saleStatus = 1)
+    : (saleStatus = 0);
+  const [isSelected, setIsSelected] = useState(false);
+  const [isFavourited, setIsFavourited] = useState(false);
+
+  // Called after "Add to Favourites" clicked
+  const handleFavourite = () => {
+    setIsFavourited(true);
+  };
+
+  // Expands the list item on click
+  const handleSelection = () => {
+    if (!isSelected) {
+      setIsSelected(true);
+    } else if (isSelected) {
+      setIsSelected(false);
+    }
+  };
+
+  // Storing the game in custom backend
+  useEffect(() => {
+    if (isFavourited) {
+      const payload = {
+        _gameId: `${props.steam_appid}`,
+        _userId: userId,
+        title: props.name,
+        genre: props.genres[0].description,
+        image: props.header_image,
+        releaseDate: props.release_date.date,
+        localPrice: props.price_overview.final,
+        publisher: props.publishers[0],
+        saleStatus: saleStatus,
+      };
+      try {
+        fetch("http://localhost/SteamService/api/steam/new-steam-game", {
+          method: "POST",
+          body: JSON.stringify(payload),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then((response) => {
+          if (!response.ok) {
+            console.warn(response.json());
+            throw new Error(`Error!  Current Status: ${response.status}!`);
+          } else if (response.ok) {
+            console.log(response.json());
+            window.location.reload();
+          }
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, [isFavourited, props, saleStatus, userId]);
+
   return (
-    <li>
-      <div>This is a searched list item.</div>
+    <li style={{ listStyle: "none" }}>
+      <div style={{ display: "flex", marginTop: "2em" }}>
+        <div
+          className={modifiers.gamesListItem}
+          onClick={handleSelection}
+          style={{ cursor: "pointer" }}
+        >
+          <span className={modifiers.gamesListAttributes}>
+            <span className={modifiers.gameListTitles}>Title:</span>{" "}
+            {props.name}
+          </span>
+          ||
+          <span className={modifiers.gamesListAttributes}>
+            <span className={modifiers.gameListTitles}>Genre:</span>{" "}
+            {props.genres[0].description}
+          </span>
+          ||
+          <span className={modifiers.gamesListAttributes}>
+            <span className={modifiers.gameListTitles}>Publisher:</span>{" "}
+            {props.publishers[0]}
+          </span>
+          ||
+          <span className={modifiers.gamesListAttributes}>
+            <span className={modifiers.gameListTitles}>Release Date:</span>{" "}
+            {props.release_date.date}
+          </span>
+          ||
+          <span className={modifiers.gamesListAttributes}>
+            <span className={modifiers.gameListTitles}>Price: </span>
+            {"R "}
+            {props.price_overview.final / 100}
+          </span>
+          ||
+          <span className={modifiers.gamesListAttributes}>
+            <span className={modifiers.gameListTitles}>Sale:</span>{" "}
+            {saleStatus === 0 ? "No" : "Yes"}
+          </span>
+        </div>
+        <button
+          className="btn btn-outline-secondary"
+          id={modifiers.btnBan}
+          style={{
+            borderRadius: "1em",
+            border: "3px solid black",
+            marginLeft: "1em",
+            paddingInlineStart: "1em",
+            paddingInlineEnd: "1em",
+          }}
+          type="button"
+          onClick={handleFavourite}
+        >
+          Favourite
+        </button>
+      </div>
+      <div
+        style={{
+          maxWidth: "70%",
+          marginTop: "1em",
+          fontFamily: "serif",
+          color: "white",
+          textShadow: "2px 0px 2px black",
+          fontSize: "large",
+        }}
+      >
+        {isSelected ? (
+          <span>
+            <span
+              className={modifiers.gameListTitles}
+              style={{ textShadow: "2px 0px 2px black" }}
+            >
+              Description:{" "}
+            </span>
+            {props.short_description}
+          </span>
+        ) : null}
+      </div>
     </li>
   );
 }
